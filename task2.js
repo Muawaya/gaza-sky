d4b1e65c912233f3fa23b96cdaad3d2bb9aa7d1d
Muawya <157010826+Muawaya@users.noreply.github.com>
2024-11-17 14:36:39 +0200

    Delete task2.js

--- task2.js
+++
@@ -1,9 +0,0 @@
-function calculateSumAndAverage(numbers) {
-    const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    const average = sum / numbers.length;
-    return { sum, average };
-}
-//________________________________________________________________
-const numbers = [10, 20, 30, 40];
-const result = calculateSumAndAverage(numbers);
-console.log(result); 
