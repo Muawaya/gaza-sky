47ac40475e153c3ddcb06fcd31bae91e18e036ae
Muawya <157010826+Muawaya@users.noreply.github.com>
2024-11-18 01:03:55 +0200

    Update task5.ts

--- product.ts/task5.ts
+++ product.ts/task5.ts
@@ -5,3 +5,20 @@ function isValidEmail(email: string): boolean {
 //------------------------------------------------------------
 console.log(isValidEmail("user@domain.com")); // true
 console.log(isValidEmail("invalid-email")); // false
+
+
+function isValidEmail(email: string): boolean {
+    const atSymbolIndex = email.indexOf("@");
+    if (atSymbolIndex === -1 || atSymbolIndex === 0 || atSymbolIndex === email.length - 1) {
+        return false;
+    }
+
+    const dotIndex = email.indexOf(".", atSymbolIndex);
+    if (dotIndex === -1 || dotIndex === atSymbolIndex + 1 || dotIndex === email.length - 1) {
+        return false;
+    }
+
+    return true; 
+}
+
+
