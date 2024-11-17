35ba4eae2c361b01b8ba9c6644fa315a6467a06e
Muawya <157010826+Muawaya@users.noreply.github.com>
2024-11-17 14:36:26 +0200

    Delete task3.js

--- task3.js
+++
@@ -1,8 +0,0 @@
-function removeDuplicates(array) {
-    const uniqueSet = new Set(array);
-    return Array.from(uniqueSet);
-}
-//_______________________________________________________________
-const items = ["apple", "banana", "apple", "orange", "banana"];
-const uniqueItems = removeDuplicates(items);
-console.log(uniqueItems);["apple", "banana", "orange"]
