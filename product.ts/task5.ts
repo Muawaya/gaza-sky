71205660a86fa966fab7e616cf1550d4196eb77d
Muawya <157010826+Muawaya@users.noreply.github.com>
2024-11-18 01:12:44 +0200

    Update task5.ts

--- product.ts/task5.ts
+++ product.ts/task5.ts
@@ -1,13 +1,4 @@
 function isValidEmail(email: string): boolean {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
-}
-//------------------------------------------------------------
-console.log(isValidEmail("user@domain.com")); // true
-console.log(isValidEmail("invalid-email")); // false
-
-
-function isValidEmail(email: string): boolean {
     const atSymbolIndex = email.indexOf("@");
     if (atSymbolIndex === -1 || atSymbolIndex === 0 || atSymbolIndex === email.length - 1) {
         return false;
